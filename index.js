1d8788bb97d0f6c3d33e2be5690b1cb08c24d925
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-07-31 17:09:38 +0300

    docs:Плоский массив

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function isUnique(string) {
-  return new Set(string).size === string.length;
+function flatten(array) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      let flat = flatten(array[i]);
+      for (let j = 0; j < flat.length; j++) {
+        result.push(flat[j]);
+      }
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
 
-console.log(isUnique("abcdef")); // true
-console.log(isUnique("1234567")); // true
-console.log(isUnique("abcABC")); // true
-console.log(isUnique("abcadef")); // false
+console.log(flatten([[1], [2, 3], [[4]]])); // -> [1,2,3,4]
