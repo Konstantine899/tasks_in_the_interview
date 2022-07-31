b7b9e4f90e67395d1560654d8d3051e9d6391668
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-07-31 19:09:45 +0300

    docs:Какая строка встречается чаще всего

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-function removeDupes(string) {
-  return Array.from(new Set(string)).join("");
+function highestFrequency(array) {
+  const map = {}; // Ключ это char, а значение сколько раз встречается
+  let maxFrequency = 0;
+  let maxFrequencyString = array[0];
+  for (let i = 0; i < array.length; i++) {
+    const current = array[i];
+    if (map[current]) {
+      map[current]++;
+    } else {
+      map[current] = 1;
+    }
+    if (map[current] > maxFrequency) {
+      maxFrequency = map[current];
+      maxFrequencyString = current;
+    }
+  }
+  return maxFrequencyString;
 }
 
-console.log(removeDupes("abcd")); // -> 'abcd'
-console.log(removeDupes("aabbccdd")); // -> 'abcd'
-console.log(removeDupes("abcddcba")); // -> 'abcd'
-console.log(removeDupes("abababcdcdcd")); // -> 'abcd'
+console.log(highestFrequency(["a", "b", "c", "c", "d", "e"])); // -> c
+console.log(highestFrequency(["abc", "dfe", "abc", "dfe", "abc"])); // -> abc
+console.log(highestFrequency(["abc", "dfe"])); // -> abc
+console.log(
+  highestFrequency([
+    "abc",
+    "abc",
+    "dfe",
+    "dfe",
+    "dfe",
+    "ghi",
+    "ghi",
+    "ghi",
+    "ghi",
+  ])
+); // -> ghi
