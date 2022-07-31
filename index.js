dd7cdac29a7d8276fddffd30c31ab3d37d04c2e3
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-07-31 23:18:10 +0300

    docs:Перевернута ли строка

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
-function highestFrequency(array) {
-  const map = {}; // Ключ это char, а значение сколько раз встречается
-  let maxFrequency = 0;
-  let maxFrequencyString = array[0];
-  for (let i = 0; i < array.length; i++) {
-    const current = array[i];
-    if (map[current]) {
-      map[current]++;
-    } else {
-      map[current] = 1;
-    }
-    if (map[current] > maxFrequency) {
-      maxFrequency = map[current];
-      maxFrequencyString = current;
-    }
-  }
-  return maxFrequencyString;
+function isStringRotated(source, test) {
+  return (source + source).includes(test) && source.length === test.length;
 }
 
-console.log(highestFrequency(["a", "b", "c", "c", "d", "e"])); // -> c
-console.log(highestFrequency(["abc", "dfe", "abc", "dfe", "abc"])); // -> abc
-console.log(highestFrequency(["abc", "dfe"])); // -> abc
-console.log(
-  highestFrequency([
-    "abc",
-    "abc",
-    "dfe",
-    "dfe",
-    "dfe",
-    "ghi",
-    "ghi",
-    "ghi",
-    "ghi",
-  ])
-); // -> ghi
+console.log(isStringRotated("javascript", "scriptjava")); // -> true
+console.log(isStringRotated("javascript", "iptjavascr")); // -> true
+console.log(isStringRotated("javascript", "java")); // -> false
