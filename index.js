c617ad08cee5f8c40c7a07bcdef6c782b983bc59
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-08-01 17:08:49 +0300

    docs:Перевернуть матрицу 3Ñx3

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-function allAnagrams(array) {
-  const sorted = array.map((string) => string.split("").sort().join(""));
-  for (let i = 0; i < sorted.length; i++) {
-    if (sorted[i] !== sorted[0]) {
-      return false;
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+/*
+ * 0:0 0:1 0:2     0:2 1:2 2:2
+ * 1:0 1:1 1:2  -> 0:1 1:1 2:1
+ * 2:0 2:1 2:2     0:0 1:0 2:0
+ * */
+
+function rotate(source) {
+  const rotated = source[0].map((_) => []);
+
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < source[i].length; j++) {
+      const value = source[i][j];
+      rotated[j][source.length - 1 - i] = value;
     }
   }
-  return true;
+  return rotated;
 }
 
-console.log(allAnagrams(["abcd", "dbac", "cabd"])); // true
-console.log(allAnagrams(["abcd", "dbXc", "cabd"])); // false
+function rotate180(source) {
+  return rotate(rotate(source));
+}
+
+function rotate270(source) {
+  return rotate(rotate180(source));
+}
+
+//helper function
+function print(array) {
+  array.forEach((i) => console.log(i));
+}
+
+print(rotate(matrix));
+console.log("--------------");
+print(rotate180(matrix));
+console.log("--------------");
+
+print(rotate270(matrix));
