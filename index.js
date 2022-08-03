8b82f5d9ff1c0a6c85ca1d4e89d90387fa6f8f00
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-08-03 13:27:23 +0300

    docs:Универсальная сумма

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-// const person = {
-//   name: "Константин",
-// };
-//
-// function info(phone, email) {
-//   console.log(`Имя: ${this.name}, Тел: ${phone}, Email:${email}`);
-// }
-//
-// //3. ES5
-// function bind(fn, context, ...rest) {
-//   return function (...args) {
-//     return fn.apply(context, rest.concat(args));
-//     // или же через call
-//     // return fn.call(context, ...rest.concat(args));
-//   };
-// }
-//
-// bind(info, person)("111", "222");
-// bind(info, person, "111")("222");
-// bind(info, person, "111", "222")();
-
-const person = {
-  name: "Константин",
-};
-
-function info(phone, email) {
-  console.log(`Имя: ${this.name}, Тел: ${phone}, Email:${email}`);
+function add(a, b) {
+  if (!a) return add;
+  if (!b) {
+    return function sum(c) {
+      if (!c) return sum;
+      return a + c;
+    };
+  }
+  return a + b;
 }
 
-function call(fn, context, args) {
-  const uniqId = Date.now().toString();
-  context[uniqId] = fn;
-  const result = context[uniqId](...args);
-  delete context[uniqId];
-  return result;
-}
-call(info, person, ["111", "222"]); // не вызываю функцию потому что метод apply сразу же ее вызывает
+console.log(add(20, 22)); // -> 42
+console.log(add(20)(22)); // -> 42
+
+console.log(add(20)()(22)); // -> 42
+console.log(add(20)()()()(22)); // -> 42
+console.log(add(20)()()()()()()()()()()()(22)); // -> 42
+
+console.log(add()(20)(22)); // -> 42
+console.log(add()()()()(20)(22)); // -> 42
+console.log(add()(20)()(22)); // -> 42
+console.log(add()()()()()(20)()()()(22)); // -> 42
